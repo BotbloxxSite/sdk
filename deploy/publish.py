b0903fb0f78204f6b3fd564ecce7671e8f6cf050
Botblox <jekychenyo@gmail.com>
2026-02-07 08:52:34 +0700

    Create publish.py

---
+++ deploy/publish.py
@@ -0,0 +1,5 @@
+def publish_game(concept, lua_files):
+    print("Publishing game:")
+    print("Title:", concept["title"])
+    print("Files:", lua_files)
+    print("Status: Uploaded to Roblox (mock)")
